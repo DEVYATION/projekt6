bdbeb98fe5e24135b18ba45bbc53a25937be3ce8
Boross Máté <71833436+MatthewBoross@users.noreply.github.com>
2021-05-02 22:49:25 +0200

    További bővítések
    Bővítések. Hiányzik még az exportáció és nagyon kevés folyamatjelző van. Jól jönne egy, a program töltését mutató ablak is.

--- HardwareSoftwareMonitor/HardwareSoftwareMonitor/MainWindow.xaml.cs
+++ HardwareSoftwareMonitor/HardwareSoftwareMonitor/MainWindow.xaml.cs
@@ -14,6 +14,46 @@ namespace HardwareSoftwareMonitor
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
+    class Drivers
+    {
+        public string DriverFriendlyName { get; set; }
+        public string DriverDescription { get; set; }
+        public string DriverVersion { get; set; }
+        public string DriverLocation { get; set; }
+        public string DriverDriverName { get; set; }
+        public string DriverManufacturer { get; set; }
+        public string DriverProvider { get; set; }
+        public string DriverCompatID { get; set; }
+        public string DriverDeviceID { get; set; }
+        public string DriverInfName { get; set; }
+
+        public Drivers(ManagementObject obj)
+        {
+            DriverFriendlyName = Convert.ToString(obj["FriendlyName"]);
+            DriverDescription = Convert.ToString(obj["CompatID"]);
+            DriverVersion = Convert.ToString(obj["DriverVersion"]);
+            DriverLocation = Convert.ToString(obj["Location"]);
+            DriverDriverName = Convert.ToString(obj["DriverName"]);
+            DriverManufacturer = Convert.ToString(obj["Manufacturer"]);
+            DriverProvider = Convert.ToString(obj["DriverProviderName"]);
+            DriverCompatID = Convert.ToString(obj["CompatID"]);
+            DriverDeviceID = Convert.ToString(obj["DeviceID"]);
+            DriverInfName = Convert.ToString(obj["InfName"]);
+        }
+    }
+
+    class Programs
+    {
+        public string ProgramName { get; set; }
+        public string ProgramVersion { get; set; }
+
+        public Programs(ManagementObject obj)
+        {
+            ProgramName = Convert.ToString(obj["Name"]);
+            ProgramVersion = Convert.ToString(obj["Version"]);
+        }
+    }
 
     public partial class MainWindow : Window
     {
@@ -29,6 +69,23 @@ namespace HardwareSoftwareMonitor
             tick.Tick += Tick_Tick;
             tick.Start();
 
+            ManagementObjectSearcher myBaseBoardObject = new ManagementObjectSearcher("SELECT * FROM Win32_BaseBoard");
+
+            foreach (ManagementObject obj in myBaseBoardObject.Get())
+            {
+                MBManufacturer.Content = "Gyártó: " + obj["Manufacturer"];
+                MBProduct.Content = "Termék neve: " + obj["Product"];
+                MBSerial.Content = "Sorozatszám: " + obj["SerialNumber"];
+            }
+
+            ManagementObjectSearcher myMotherboardObject = new ManagementObjectSearcher("select * from Win32_MotherboardDevice");
+
+            foreach (ManagementObject obj in myMotherboardObject.Get())
+            {
+                MBSysName.Content = "Rendszer neve: " + obj["SystemName"];
+                MBbus.Content = "Busz típusa: " + obj["PrimaryBusType"];
+            }
+
             ManagementObjectSearcher myProcessorObject = new ManagementObjectSearcher("select * from Win32_Processor");
 
             foreach (ManagementObject obj in myProcessorObject.Get())
@@ -40,6 +97,36 @@ namespace HardwareSoftwareMonitor
                 CPUCores.Content = "Magok száma: " + obj["NumberOfCores"];
                 CPUArch.Content = "Architektúra: " + obj["Architecture"];
                 CPUFamily.Content = "Család: " + obj["Family"];
+            }
+
+            ManagementObjectSearcher myMemoryObject = new ManagementObjectSearcher("SELECT * FROM Win32_PhysicalMemory");
+            int db = 1;
+
+            foreach (ManagementObject obj in myMemoryObject.Get())
+            {
+                TabItem tab = new TabItem();
+                tab.Header = db + ". modul";
+                Memory.Items.Add(tab);
+                Grid grid = new Grid();
+                grid.Background = new SolidColorBrush(Color.FromRgb(229, 229, 229));
+                Label l1 = new Label();
+                l1.Margin = new Thickness(10, 10, 0, 0);
+                l1.Content = "Kapacitás: " + Math.Round(Convert.ToInt64(obj["Capacity"]) / 1073741824.0, 2) + "GB";
+                Label l2 = new Label();
+                l2.Margin = new Thickness(10, 40, 0, 0);
+                l2.Content = "Gyártó: " + obj["Manufacturer"];
+                Label l3 = new Label();
+                l3.Margin = new Thickness(10, 70, 0, 0);
+                l3.Content = "Formai tényező: " + obj["FormFactor"];
+                Label l4 = new Label();
+                l4.Margin = new Thickness(10, 100, 0, 0);
+                l4.Content = "Típus: " + obj["MemoryType"];
+                grid.Children.Add(l1);
+                grid.Children.Add(l2);
+                grid.Children.Add(l3);
+                grid.Children.Add(l4);
+                tab.Content = grid;
+                db++;
             }
 
             DriveInfo[] allDrives = DriveInfo.GetDrives();
@@ -139,7 +226,12 @@ namespace HardwareSoftwareMonitor
 
             if (nics == null || nics.Length < 1)
             {
-                
+                NetGrid.Children.Clear();
+                Label l = new Label();
+                l.Margin = new Thickness(20, 20, 0, 0);
+                l.FontSize = 18;
+                l.Content = "Nincs semmilyen hálózati kapcsolat.";
+                NetGrid.Children.Add(l);
             }
             else
             {
@@ -187,21 +279,13 @@ namespace HardwareSoftwareMonitor
                 l1.Content = "Termék neve: " + obj["ProductName"];
                 Label l2 = new Label();
                 l2.Margin = new Thickness(10, 50, 0, 0);
-                l2.Content = "Elérhetőség: " + obj["Availability"];
+                l2.Content = "Eszköz ID-je: " + obj["DeviceID"];
                 Label l3 = new Label();
                 l3.Margin = new Thickness(10, 80, 0, 0);
-                l3.Content = "Eszköz ID-je: " + obj["DeviceID"];
-                Label l4 = new Label();
-                l4.Margin = new Thickness(10, 110, 0, 0);
-                l4.Content = "Támogatott-e az energiamenedzsment: " + obj["PowerManagementSupported"];
-                Label l5 = new Label();
-                l5.Margin = new Thickness(10, 140, 0, 0);
-                l5.Content = "Állapot: " + obj["Status"];
+                l3.Content = "Állapot: " + obj["Status"];
                 grid.Children.Add(l1);
                 grid.Children.Add(l2);
                 grid.Children.Add(l3);
-                grid.Children.Add(l4);
-                grid.Children.Add(l5);
                 tab.Content = grid;
             }
 
@@ -216,24 +300,20 @@ namespace HardwareSoftwareMonitor
                 grid.Background = new SolidColorBrush(Color.FromRgb(229, 229, 229));
                 Label l1 = new Label();
                 l1.Margin = new Thickness(10, 10, 0, 0);
-                l1.Content = "Hálózati nyomtató-e: " + obj["Network"];
+                l1.Content = "Alapértelmezett nyomtató: " + obj["Default"];
                 Label l2 = new Label();
                 l2.Margin = new Thickness(10, 40, 0, 0);
-                l2.Content = "Elérhetőség: " + obj["Availability"];
+                l2.Content = "Hálózati nyomtató-e: " + obj["Network"];
                 Label l3 = new Label();
                 l3.Margin = new Thickness(10, 70, 0, 0);
                 l3.Content = "Eszköz ID-je: " + obj["DeviceID"];
                 Label l4 = new Label();
                 l4.Margin = new Thickness(10, 100, 0, 0);
-                l4.Content = "Alapértelmezett nyomtató: " + obj["Default"];
-                Label l5 = new Label();
-                l5.Margin = new Thickness(10, 130, 0, 0);
-                l5.Content = "Állapot: " + obj["Status"];
+                l4.Content = "Állapot: " + obj["Status"];
                 grid.Children.Add(l1);
                 grid.Children.Add(l2);
                 grid.Children.Add(l3);
                 grid.Children.Add(l4);
-                grid.Children.Add(l5);
                 tab.Content = grid;
             }
 
@@ -243,9 +323,23 @@ namespace HardwareSoftwareMonitor
             {
                 OSType.Content = "Operációs rendszer: " + obj["Caption"];
                 OSWinDir.Content = "Windows mappa helye: " + obj["WindowsDirectory"];
-                OSSerial.Content = "Sorozatszám: " + obj["SerialNumber"];
                 OSSysDir.Content = "Rendszermappa helye: " + obj["SystemDirectory"];
+                OSSerial.Content = "Sorozatszám: " + obj["SerialNumber"];
                 OSVersion.Content = "Verzió: " + obj["Version"];
+            }
+
+            ManagementObjectSearcher driverSearch = new ManagementObjectSearcher("select * from Win32_PnPSignedDriver");
+
+            foreach (ManagementObject service in driverSearch.Get())
+            {
+                DriverGrid.Items.Add(new Drivers(service));
+            }
+
+            ManagementObjectSearcher programSearch = new ManagementObjectSearcher("select * from Win32_Product");
+
+            foreach (ManagementObject obj in programSearch.Get())
+            {
+                ProgramsGrid.Items.Add(new Programs(obj));
             }
         }
 
